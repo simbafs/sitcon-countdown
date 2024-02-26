c201e5d86f2d8a27367499a30ff6f40771b897ed
SimbaFs <simba.fs@gmail.com>
2024-02-26 20:25:14 +0800

    format sessin type

--- backend/models/sessions/sessions.go
+++ backend/models/sessions/sessions.go
@@ -8,11 +8,17 @@ import (
 type input struct {
 	Sessions []Session `json:"sessions"`
 	Speakers []struct {
-		Id string
+		Id string `json:"id"`
 		Zh struct {
-			Name string
-		}
+			Name string `json:"name"`
+		} `json:"zh"`
 	} `json:"speakers"`
+	SessionsTypes []struct {
+		Id string `json:"id"`
+		Zh struct {
+			Name string `json:"name"`
+		} `json:"zh"`
+	} `json:"session_types"`
 }
 
 type Session struct {
@@ -38,6 +44,13 @@ func ParseSessions(data []byte) (map[string]Session, error) {
 		speakers[speaker.Id] = speaker.Zh.Name
 	}
 
+	// build map of session type and its name
+	typeString := make(map[string]string)
+	for _, t := range s.SessionsTypes {
+		typeString[t.Id] = t.Zh.Name
+	}
+
+	// clear sessions information
 	result := make(map[string]Session)
 
 	for sessionId, session := range s.Sessions {
@@ -58,6 +71,13 @@ func ParseSessions(data []byte) (map[string]Session, error) {
 			return nil, err
 		}
 		s.Sessions[sessionId].End = end.Format("15:04")
+
+		// type
+		t, ok := typeString[session.Type]
+		if !ok {
+			t = session.Type
+		}
+		s.Sessions[sessionId].Type = t
 
 		result[session.Id] = s.Sessions[sessionId]
 	}
