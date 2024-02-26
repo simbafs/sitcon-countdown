b38384140f42f45d59b5a767ed9e0e898247c266
SimbaFs <simba.fs@gmail.com>
2024-02-26 21:46:13 +0800

    update session.StartTime and session.EndTime in POST /api/card

--- backend/api/card/card.go
+++ backend/api/card/card.go
@@ -1,10 +1,11 @@
 package card
 
 import (
-	"backend/models/sessions"
+	"backend/models/session"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,13 +15,19 @@ type Time struct {
 	End   string `json:"end"`
 }
 
+// PareseTime parse string like "11:30" to time.Time, whose year, month and day are 2024-03-09
+func ParseTime(t string) (time.Time, error) {
+	t = "2024-03-09 " + t + ":00"
+	return time.Parse("2006-01-02 15:04:05", t)
+}
+
 func Route(r gin.IRouter) {
 	file, err := os.ReadFile("sessions.json")
 	if err != nil {
 		panic(err)
 	}
 
-	sessions, err := sessions.ParseSessions(file)
+	sessions, err := session.ParseSessions(file)
 
 	route := r.Group("/card")
 
@@ -36,6 +43,22 @@ func Route(r gin.IRouter) {
 		c.JSON(http.StatusOK, session)
 	})
 
+	route.GET("/room/:roomid", func(c *gin.Context) {
+		roomid := c.Param("roomid")
+
+		roomSession := session.Session{}
+
+		for _, s := range sessions {
+			if s.Room != roomid {
+				continue
+			}
+
+			if time.Now().Before(s.EndTime) && s.StartTime.Before(roomSession.StartTime) {
+				roomSession = s
+			}
+		}
+	})
+
 	route.POST("/:id", func(c *gin.Context) {
 		id := c.Param("id")
 
@@ -49,6 +72,15 @@ func Route(r gin.IRouter) {
 
 		s.Start = t.Start
 		s.End = t.End
+
+		s.StartTime, err = ParseTime(t.Start)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid start time"})
+		}
+		s.EndTime, err = ParseTime(t.End)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid end time"})
+		}
 
 		sessions[id] = s
 
