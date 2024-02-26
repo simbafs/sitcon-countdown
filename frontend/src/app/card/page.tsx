2c7a54c285b7e50f17dda682b74b9eadf5afb895
SimbaFs <simba.fs@gmail.com>
2024-02-26 20:08:56 +0800

    update page /card

--- frontend/src/app/card/page.tsx
+++ frontend/src/app/card/page.tsx
@@ -2,18 +2,25 @@
 
 import useQuery from "@/hooks/useQuery"
 import useSWR from 'swr'
+import { useEffect } from "react"
 
-export type Subtitle = {
+export type Card = {
     type: string
-    title: string
+    zh: {
+        title: string
+    }
     start: string
     end: string
-    talker: string
+    speakers: string[]
 }
 
 export default function Page() {
     const sessionId = useQuery('id')
-    const { data, error } = useSWR<Subtitle>(`/api/subtitle/${sessionId}`, url => fetch(url).then(res => res.json()))
+    const { data, error } = useSWR<Card>(`/api/card/${sessionId}`, url => fetch(url).then(res => res.json()), {
+        refreshInterval: 500,
+    })
+
+    useEffect(() => console.log(data), [data])
 
     if (error) return <>
         <h1>Error</h1>
@@ -22,12 +29,12 @@ export default function Page() {
 
     if (!data) return <h1>Loading...</h1>
 
+
     // return <pre>{JSON.stringify(data, null, 2)}</pre>
     return <div className="flex flex-col gap-4">
         <h2 className="text-[8vw]">{data.type}</h2>
-        <h1 className="text-[10vw]">{data.title}</h1>
+        <h1 className="text-[10vw]">{data.zh.title}</h1>
         <p className="text-[6vw]">{data.start}-{data.end}</p>
-        <p className="text-[6vw] text-right">--{data.talker}</p>
-
+        {data.speakers.map(speaker => <p key={speaker} className="text-[6vw]">{speaker}</p>)}
     </div>
 }
