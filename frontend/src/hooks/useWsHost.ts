ee6cf1e3973df864af676d09ef77d4b2678c9ff1
SimbaFs <simba.fs@gmail.com>
2024-02-27 23:12:40 +0800

    fix bug in ws connection when useing https

--- frontend/src/hooks/useWsHost.ts
+++ frontend/src/hooks/useWsHost.ts
@@ -4,10 +4,10 @@ export default function useWsHost() {
 	const [ws, setWs] = useState<null | string>(null)
 
 	useEffect(() => {
-		const protocal = window.location.protocol === 'https://' ? 'wss://' : 'ws://'
+		const protocal = window.location.protocol === 'https:' ? 'wss:' : 'ws:'
 		const host = window.location.host
 
-		setWs(`${protocal}${host}/ws`)
+		setWs(`${protocal}//${host}/ws`)
 	}, [])
 
 	return ws
